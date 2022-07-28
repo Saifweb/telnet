fd0bfb64848e234bdf6ed1b0fdedfdf6c64b0a1d
Saifweb <seifeddine.benhamida@supcom.tn>
2022-07-28 13:18:42 +0200

    first

---
+++ server.js
@@ -0,0 +1,35 @@
+//set express to work
+const express =require('express');
+const app=express();
+
+//set port 
+app.listen(5000);
+//set index.html
+app.set('views','./Views');
+//set static file index+css
+app.use(express.static('public'));
+app.use(express.static(__dirname + '/Views'));
+app.use('/css',express.static(__dirname + '/public'));
+app.use('/js',express.static(__dirname + '/public'));
+//set WebSocket 
+const WebSocket=require('ws');
+// open connection with port ! 
+const UrlSocket = new WebSocket.Server({port: 8082});
+// test connection usless for our case ( if we dont use the url : ws://localhost:8082 )
+UrlSocket.on('connection', socket =>{
+
+    console.log(" new client connection open");
+
+    socket.on("message",data=>{
+        console.log(data);
+        socket.send(data);
+    })
+    socket.on('close',()=>{
+        console.log('client disconnected');
+    })
+
+})
+
+
+
+
